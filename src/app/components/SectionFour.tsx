7596af4ed627434b1955894dc33a2b9e18a02335
Vinicius Lemes <viniciuslemesds@gmail.com>
2024-09-04 20:52:22 -0300

    update to the PC images

--- src/app/components/SectionFour.tsx
+++ src/app/components/SectionFour.tsx
@@ -18,34 +18,53 @@ export default function SectionTwo() {
   return (
     <>
       <motion.div
-        className="bg-slate-100 pt-12 tablet:pt-0 h-fit pb-3 tablet:h-[70dvh] flex flex-col tablet:flex-row justify-center items-center gap-2"
+        className="bg-slate-100 tablet:pt-0 m-3 pb-3 tablet:h-[70dvh] flex flex-col justify-center items-center "
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
       >
-        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.2}}>
-          <StepCard
-            title="1. Zero Class"
-            description="Talk to your new student and decide what module suits them best. Decide what time the classes are going to take place and how many times a week."
-          />
-        </motion.div>
-        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.5}}>
-          <StepCard
-            title="2. Add Student"
-            description="Go to the Students tab on the Teacher's Dashboard and click on Add Student, include the Student's Details and save. "
-          />
-        </motion.div>
-        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 1}}>
-          <StepCard
-            title="3. First Login"
-            description="Make sure to use the student's Gmail address when filling in the Student's Details and tell them to login using their Gmail Account."
-          />
-        </motion.div>
-        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 1.5}}>
-          <StepCard
-            title="Done!"
-            description="You're ready to start your classes! You can edit the Student's Details at any time to go to grant them access to a new module or change their email account."
-          />
-        </motion.div>
+        <span className="font-bold text-xl tablet:text-2xl p-4 mt-12 text-slate-600">New Student step-by-step:</span>
+        <div className="flex flex-col mb-12 tablet:flex-row justify-center items-center gap-2">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.2 }}
+          >
+            <StepCard
+              title="1. Zero Class"
+              description="Talk to your new student and decide what module suits them best. Decide what time the classes are going to take place and how many times a week."
+            />
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.5 }}
+          >
+            <StepCard
+              title="2. Add Student"
+              description="Go to the Students tab on the Teacher's Dashboard and click on Add Student, include the Student's Details and save. "
+            />
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1 }}
+          >
+            <StepCard
+              title="3. First Login"
+              description="Make sure to use the student's Gmail address when filling in the Student's Details and tell them to login using their Gmail Account."
+            />
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.5 }}
+          >
+            <StepCard
+              title="Done!"
+              description="You're ready to start your classes! You can edit the Student's Details at any time to go to grant them access to a new module or change their email account."
+            />
+          </motion.div>
+        </div>
       </motion.div>
     </>
   );
--- src/app/components/SectionThree.tsx
+++ src/app/components/SectionThree.tsx
@@ -1,27 +1,54 @@
-"use client"
+"use client";
 import Image from "next/image";
-import PC from "../../../public/s3pcs.png";
+import PC from "../../../public/Group 1.svg"; // Ensure this is the correct path
+import PC1 from "../../../public/Group 2dash.svg"
+import PC2 from "../../../public/Group 3stds.svg"
+import PC3 from "../../../public/Group 1cr.svg"
 import { motion } from "framer-motion";
+import Carousel from "react-bootstrap/Carousel";
+import { useState } from "react";
 
 export default function SectionThree() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex: number) => {
+    setIndex(selectedIndex);
+  };
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      className="relative flex flex-col bg-white h-80 tablet:h-screen w-full items-center justify-center p-1 tablet:p-12"
+      className="relative flex flex-col bg-white w-full h-80 tablet:h-screen  items-center justify-center p-1 tablet:p-12"
     >
-      <span className="text-4xl mt-12 font-bold text-[#0A3053]">
-        Teacher{"'"}s Dashboard
-      </span>
-      <div className="relative w-full h-full">
-        <Image
-          src={PC}
-          alt=""
-          layout="fill"
-          objectFit="contain"
-          style={{ maxHeight: "100%", maxWidth: "100%" }}
-        />
-      </div>
+      {/* <span className="text-white font-bold text-3xl pt-6">Teacher{"'"}s Dashboard</span> */}
+      <Carousel
+        activeIndex={index}
+        onSelect={handleSelect}
+        className="w-full flex "
+      >
+        <Carousel.Item>
+          <div className="relative w-full h-full p-10 flex flex-col items-center justify-center">
+            <span className="font-bold text-2xl text-blue-600 border-b-2">Teacher{"'"}s Dashboard</span>
+            <br />
+            <Image src={PC1} alt="First slide" width={600} height={600} />
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className="relative w-full h-full p-10 flex flex-col items-center justify-center">
+            <span className="font-bold text-2xl text-blue-600 border-b-2">Students Table</span>
+            <br />
+            <Image src={PC2} alt="First slide" width={600} height={600} />
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className="relative w-full h-full p-10 flex flex-col items-center justify-center">
+            <span className="font-bold text-2xl text-blue-600 border-b-2">Class Records</span>
+            <br />
+            <Image src={PC3} alt="First slide" width={600} height={600} />
+          </div>
+        </Carousel.Item>
+      </Carousel>
     </motion.div>
   );
 }
